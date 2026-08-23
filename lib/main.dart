2839d5863c91bc82cd431bdb85a0c3a3ae413646
teameduva-cell <teameduva@users.noreply.github.com>
2026-08-23 15:49:43 +0000

    Direct launch OnboardingScreen test

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,8 @@ import 'features/onboarding/presentation/screens/onboarding_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const EduvaApp());
-}
-
-class EduvaApp extends StatelessWidget {
-  const EduvaApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: OnboardingScreen(),
-    );
-  }
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: OnboardingScreen(),
+  ));
 }
